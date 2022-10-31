d5ad1eb0897f4cc0e6db781681101cff8335197b
Singh, Sujit <sujit_singh2@epam.com>
2022-10-31 09:54:16 +0530

    Task 5 - Added S3 integration for file import

---
+++ import-service/src/functions/importFileParser/index.ts
@@ -0,0 +1,19 @@
+// import schema from './schema';
+import { handlerPath } from '@libs/handler-resolver';
+
+export default {
+  handler: `${handlerPath(__dirname)}/handler.main`,
+  events: [
+    {
+      s3: {
+        bucket: 'uploaded-jscc',
+        event: 's3:ObjectCreated:*',
+        rules: [{
+          prefix: 'uploaded/'
+        }],
+        existing: true,
+        cors: true,
+      },
+    },
+  ],
+};
